6aaf0f05920e25a2e6e9704cbdea8a3823459ef8
jngbn <[user email]>
2025-10-27 20:21:11 +0900

    Initial commit from IntelliJ

---
+++ src/main/java/precourse/mvcPractiec/MvcPractiecApplication.java
@@ -0,0 +1,13 @@
+package precourse.mvcPractiec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcPractiecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcPractiecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/precourse/mvcPractiec/MvcPractiecApplicationTests.java
@@ -0,0 +1,13 @@
+package precourse.mvcPractiec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcPractiecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
